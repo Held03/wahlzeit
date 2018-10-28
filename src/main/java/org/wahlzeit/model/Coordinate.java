361aa69473fbd49d1beca6bd1cb405ff77da2a09
Cryptjar <cryptjar@wag-web.de>
2018-10-28 15:17:39 +0100

    Implement coordinate methods

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -4,7 +4,7 @@
 package org.wahlzeit.model;
 
 /**
- * A coordinate is a mathematical Cartesian location.
+ * A coordinate is an immutable mathematical Cartesian point.
  */
 public class Coordinate {
 
@@ -12,9 +12,9 @@ public class Coordinate {
 	 * 
 	 */
 	public static final Coordinate ORIGIN = new Coordinate(0.0, 0.0, 0.0);
-	public static final Coordinate UINT_X = new Coordinate(1.0, 0.0, 0.0);
-	public static final Coordinate UINT_Y = new Coordinate(0.0, 1.0, 0.0);
-	public static final Coordinate UINT_Z = new Coordinate(0.0, 0.0, 1.0);
+	public static final Coordinate UNIT_X = new Coordinate(1.0, 0.0, 0.0);
+	public static final Coordinate UNIT_Y = new Coordinate(0.0, 1.0, 0.0);
+	public static final Coordinate UNIT_Z = new Coordinate(0.0, 0.0, 1.0);
 
 	/*
 	 * 
@@ -33,5 +33,83 @@ public class Coordinate {
 		this.y = y;
 		this.z = z;
 	}
+	
+	/**
+	 * Return the Euclidean distance between this and the given coordinate.
+	 * 
+	 * @param other the other coordinate to compare
+	 * @return the distance
+	 */
+	public double getDistance(Coordinate other) {
+		double dx = this.x - other.x;
+		double dy = this.y - other.y;
+		double dz = this.z - other.z;
+		
+		double sqDist = dx*dx + dy*dx + dz*dz;
+		
+		return Math.sqrt(sqDist);
+	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(z);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coordinate other = (Coordinate) obj;
+		return isEqual(other);
+	}
+	
+	public boolean isEqual(Coordinate other) {
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		if (Double.doubleToLongBits(z) != Double.doubleToLongBits(other.z))
+			return false;
+		return true;
+	}
+
+	/**
+	 * @return the x
+	 * @methodtype get
+	 */
+	public double getX() {
+		return x;
+	}
+
+	/**
+	 * @return the y
+	 * @methodtype get
+	 */
+	public double getY() {
+		return y;
+	}
+
+	/**
+	 * @return the z
+	 * @methodtype get
+	 */
+	public double getZ() {
+		return z;
+	}
+
+	
+	
 }
