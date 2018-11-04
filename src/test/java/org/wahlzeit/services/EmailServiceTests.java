39b5083eca3d58023fa3074eabcb22e7291b4744
Held03 <held03@wag-web.de>
2018-11-04 20:27:49 +0100

    Add a email service test suit

---
+++ src/test/java/org/wahlzeit/services/EmailServiceTests.java
@@ -0,0 +1,12 @@
+package org.wahlzeit.services;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.wahlzeit.services.mailing.EmailServiceTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ EmailAddressTest.class, EmailServiceTest.class })
+public class EmailServiceTests {
+
+}
